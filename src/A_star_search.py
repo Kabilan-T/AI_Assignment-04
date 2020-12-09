b32236e3d1e15943feb95be46853c9d3d33f01c2
kabilan <kavikabilan37@gmail.com>
2020-12-09 18:04:26 +0530

    modified A Star search program

--- src/A_star_search.py
+++ src/A_star_search.py
@@ -15,21 +15,18 @@ from helper import *
 
 def Astar_search(board, opt):
     """Function to implement the A-star search algorithm.
-    Please use the functions in helper.py to complete the algorithm.
-    Please do not clutter the code this file by adding extra functions.
-    Additional functions if required should be added in helper.py
 
     Parameters
     ----------
-    board : [type]
-        [description]
-    opt : [type]
-        [description]
+    board : Puzzle
+        object of class Puzzle which contains the puzzle configuration
+    opt : int
+        1 for Manhattan Dist heuristic, 2 for Misplaced Tiles heuristic
 
     Returns
     -------
-    [type]
-        [description]
+    Sequence[int]
+        path from unsolved puzzle to solved puzzle
     """
 
     priority_queue = []
@@ -38,12 +35,13 @@ def Astar_search(board, opt):
     h_score = 0  # heuristic function value
 
     # Creating a heap from list to store the nodes with the priority h_score
-    heappush(priority_queue, (f_score, g_score, h_score, board.get_initial_state()))
+    depth = 0
+    heappush(priority_queue, (f_score, g_score, h_score, board.get_initial_state(),depth))
 
     # FILL IN YOUR CODE HERE
-    node_relations =list()
+    node_relations =list([("", board.get_initial_state())])
     while len(priority_queue) > 0:
-        g_score,_,_,current_node = heappop(priority_queue) 
+        _,_,_,current_node,depth = heappop(priority_queue) 
         if board.goal_test(current_node):
             path = [current_node]
             while current_node != board.initial_state:
@@ -58,21 +56,37 @@ def Astar_search(board, opt):
             break
         else:
             possible_moves = get_possible_moves(current_node,board)
+            g_score = depth
             for node in possible_moves:
                 if opt ==1 : h_score = get_manhattan_distance(node)
                 if opt ==2 : h_score = no_of_misplaced_tiles(node)
                 f_score = g_score + h_score
-                heappush(priority_queue, (f_score, g_score, h_score, node))
-                node_relations.append([current_node,node])
+                heappush(priority_queue, (f_score, g_score, h_score, node, depth+1))
+                node_relations.append((current_node,node))
     return path
 
 
-if __name__ == '__main__':       
-    puzzle_8 = [0, 1, 2, 3, 4, 5, 8, 6, 7] # Initial Configuration for testing
-    # puzzle_8 = [8, 7, 6, 5, 1, 4, 2, 0, 3] # Second Configuration for testing
-    # puzzle_8 = [1, 5, 7, 3, 6, 2, 0, 4, 8] # Final Configuration for testing
+if __name__ == '__main__':
 
-    print("Initial Configuration")
+    puzzle1 = [0, 1, 2, 3, 4, 5, 8, 6, 7] # Initial Configuration for testing
+    puzzle2 = [8, 7, 6, 5, 1, 4, 2, 0, 3] # Second Configuration for testing
+    puzzle3 = [1, 5, 7, 3, 6, 2, 0, 4, 8] # Final Configuration for testing
+    print("First configuration of puzzle")
+    print_puzzle(puzzle1)
+    print("Second configuration of puzzle")
+    print_puzzle(puzzle2)
+    print("Third configuration of puzzle")
+    print_puzzle(puzzle3)
+
+    puzzle_opt = int(input("Enter which configuration of puzzle to run (1 or 2 or 3 ) : "))
+    if   puzzle_opt == 1 : puzzle_8 = puzzle1
+    elif puzzle_opt == 2 : puzzle_8 = puzzle2
+    elif puzzle_opt == 3 : puzzle_8 = puzzle3
+    else : 
+        print("Invalid Choice")
+        sys. exit()
+        
+    print("\nSelected Configuration")
     board = Puzzle(puzzle_8)
     print_puzzle(puzzle_8)
     opt = int(sys.argv[1])
